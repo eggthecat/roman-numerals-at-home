c49b39238010df1543cbc5103b0bbfd5016ab834
Dominic Montelongo <montelnogodominic@gmail.com>
2019-04-05 08:06:01 -0700

    Add content to js

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+function convertToRoman(num) {
+  var romanToNum = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90.
+    L: %0,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+  var roman = '';
+
+  for (var key in romanToNum) {
+    console.long(key)
+    console.log('key: ', key);
+    console.log('value: ', romanToNum[key]){
+    while (num>= romanToNum[key]);
+    roman += key;
+    num -= romanToNum[key];
+    console.log("Num is decreased: ", num)
+
+  }
+}
+
+  }
+
+
+
+  convertToRoman(3)
